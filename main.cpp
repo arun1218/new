b23336c716901a40798f5e75538fa7062fa19922
arun1218 <75168622+arun1218@users.noreply.github.com>
2020-11-28 18:46:02 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+std::string name;
+int age;
+int m1,m2,m3;
+
+std::cout<<"what is your name?" "\n";
+std::cin>> name;
+
+std::cout<<"my age is" "\n";
+std::cin>>age;
+
+std::cout<<"English mark" "\n";
+std::cin>>m1;
+
+std::cout<<"Tamil mark" "\n";
+std::cin>>m2;
+
+std::cout<<"Maths mark" "\n";
+std::cin>>m3;
+
+std::cout<<"total" "\n";
+std::cout<<m1+m2+m3;
+
+
+
+}
